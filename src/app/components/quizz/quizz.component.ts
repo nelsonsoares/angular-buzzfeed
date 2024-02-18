d0792bc75b0967872b6d77ec64bf0c9a7c5695af
nelsonsoares <nelson.nsf40@gmail.com>
2024-02-18 16:49:34 -0300

    feat: project finished

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,12 +1,62 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import quiz_questions from '../../../assets/data/quizz_questions.json';
 @Component({
   selector: 'app-quizz',
   standalone: true,
   imports: [],
   templateUrl: './quizz.component.html',
-  styleUrl: './quizz.component.scss'
+  styleUrl: './quizz.component.scss',
 })
-export class QuizzComponent {
+export class QuizzComponent implements OnInit {
+  title: string = '';
+  questions: any;
+  questionSelected: any;
+  answers: string[] = [];
+  answersSelected: string = '';
+  questionIndex: number = 0;
+  questionMaxIndex: number = 0;
+  finished: boolean = false;
 
+  ngOnInit(): void {
+    if (quiz_questions) {
+      this.finished = false;
+      this.title = quiz_questions.title;
+      this.questions = quiz_questions.questions;
+      this.questionSelected = this.questions[this.questionIndex];
+      this.questionIndex = 0;
+      this.questionMaxIndex = this.questions.length;
+    }
+  }
+
+  playerChoose(value: string) {
+    this.answers.push(value);
+    this.nextStep();
+  }
+
+  async nextStep() {
+    this.questionIndex++;
+
+    if (this.questionMaxIndex > this.questionIndex) {
+      this.questionSelected = this.questions[this.questionIndex];
+    } else {
+      const finalAnswer: string = await this.checkResult(this.answers);
+      this.finished = true;
+      this.answersSelected = quiz_questions.results[finalAnswer as keyof typeof quiz_questions.results];
+    }
+  }
+
+  async checkResult(answers: string[]) {
+    const result = answers.reduce((previous, current, index, arr) => {
+      if (
+        arr.filter((item) => item === previous).length >
+        arr.filter((item) => item === current).length
+      ) {
+        return previous;
+      } else {
+        return current;
+      }
+    });
+
+    return result;
+  }
 }
